68b7e24a63de11a4e9a7911bf9b7202cdb6a95e2
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-05-21 10:36:26 +0530

    terroform

--- tfvars/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- 
-  backend "s3" {
-    bucket         = "kalyan-s3-dev"
-    key            = "multi-env-tfvars"
-    region         = "us-east-1"
-    dynamodb_table = "kalyan-Ddb-dev"
-  }
-}
--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -2,3 +2,4 @@ bucket = "kalyan-s3-prod"
 key    = "multi-env-tfvars"
 region = "us-east-1"
 dynamodb_table = "kalyan-Ddb-prod"
+
